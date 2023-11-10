690542582b2c9cb427d39580929e13554ab797f6
ira089 <gvir2015@gmail.com>
2023-11-10 16:49:36 +0200

    js09_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+// console.dir(refs.btnStart);
+// console.dir(refs.btnStop);
+// console.dir(refs.bodyColor);
 
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+let timerId = null;
+
+function onBtnStart() {
+  //   console.log(evt);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    // console.log('qw');
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStop() {
+  //   console.log(evt);
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
